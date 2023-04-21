83e87c86c7e0f219bd3be34312e5cebe4cbea4d9
Luke Allcock <121309748+lukeallcodes@users.noreply.github.com>
2023-04-21 14:59:22 -0700

    Update BubbleSort.js

--- SortingAlgosJavascript/BubbleSort.js
+++ SortingAlgosJavascript/BubbleSort.js
@@ -1,1 +1,14 @@
-
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap arr[j] and arr[j+1]
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
