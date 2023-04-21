56c2fb8ec2080cb57199390d9a39b169407bc2e1
Luke Allcock <121309748+lukeallcodes@users.noreply.github.com>
2023-04-21 15:10:06 -0700

    Update RadixSort.js

--- SortingAlgosJavascript/RadixSort.js
+++ SortingAlgosJavascript/RadixSort.js
@@ -1,1 +1,45 @@
+function radixSort(arr) {
+  // Find the maximum number to know the number of digits
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
 
+  // Do counting sort for every digit from least significant digit to most significant digit
+  for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
+    countingSort(arr, exp);
+  }
+
+  return arr;
+}
+
+function countingSort(arr, exp) {
+  const n = arr.length;
+  const output = new Array(n);
+  const count = new Array(10).fill(0);
+
+  // Store count of occurrences in count[]
+  for (let i = 0; i < n; i++) {
+    const digit = Math.floor(arr[i] / exp) % 10;
+    count[digit]++;
+  }
+
+  // Change count[i] so that it contains actual position of this digit in output[]
+  for (let i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
+
+  // Build the output array
+  for (let i = n - 1; i >= 0; i--) {
+    const digit = Math.floor(arr[i] / exp) % 10;
+    output[count[digit] - 1] = arr[i];
+    count[digit]--;
+  }
+
+  // Copy the output array to arr[], so that arr[] now contains sorted numbers according to current digit
+  for (let i = 0; i < n; i++) {
+    arr[i] = output[i];
+  }
+}
